c2507a9d204e70a0b37227beef523dfc97f3df6e
Lincoln Kushwah <lincolnkushwah009@gmail.com>
2020-06-06 17:02:27 +0530

    ..

--- lib/Screens/horoscope.dart
+++ lib/Screens/horoscope.dart
@@ -5,8 +5,50 @@ class horoscope extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromRGBO(0, 0, 51, 100),
-      body: Container(
-          child: Image.asset("image/aries.jpg"),),
+      body: DefaultTabController(
+          length: 3,
+          child: Scaffold(
+
+
+            body: Column(
+              children: <Widget>[
+                Container(
+                  child: Image.asset('images/aries.jpg'),
+                ),
+                TabBar(indicatorSize: TabBarIndicatorSize.label, tabs: [
+                  Tab(
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text("APPS"),
+                    ),
+                  ),
+                  Tab(
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text("MOVIES"),
+                    ),
+                  ),
+                  Tab(
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text("GAMES"),
+                    ),
+                  ),
+                ]),
+                Container(
+                  width: 400,
+                  height: 200,
+                  child: TabBarView(children: [
+                    Icon(Icons.apps),
+                    Icon(Icons.movie),
+                    Icon(Icons.games),
+                  ]),
+                ),
+              ],
+            ),
+          )
+      )
+
     );
   }
 }
