d8511bdda0721990f66dabd438923e30685036dc
shreshtha <shreshthashukle209@gmail.com>
2020-06-07 13:45:21 +0530

    code

--- lib/Screens/horoscope.dart
+++ lib/Screens/horoscope.dart
@@ -3,10 +3,34 @@ import 'package:flutter/material.dart';
 class horoscope extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromRGBO(0, 0, 51, 100),
-      body: Container(
-          child: Image.asset("image/aries.jpg"),),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color.fromRGBO(0, 0, 51, 100),
+        body: new Column(
+          children: [
+            new Container(
+              child:
+              Image.asset("images/aries.jpg"),
+            ),
+            new Container(
+              child: DefaultTabController(
+                length: 3,
+                child: Scaffold(
+                  appBar: AppBar(
+                    bottom: TabBar(
+                      tabs: [
+                      Tab(title: Text('kjuygwef'),),
+                      Tab(),
+                      Tab(),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      ),
     );
   }
 }
