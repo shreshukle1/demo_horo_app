3238487a1f44986d237d345682f3b6e2ce46afd0
shreshtha <shreshthashukle209@gmail.com>
2020-06-10 17:25:04 +0530

    horo code

---
+++ lib/Screens/ContactUs.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class ContactUs extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(0, 0, 51, 100),
+    );
+  }
+}
