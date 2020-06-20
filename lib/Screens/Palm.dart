662906bd35071e36aaec41659b44fa674f8828c5
shreshtha <shreshthashukle209@gmail.com>
2020-06-20 15:54:00 +0530

    palm feature

---
+++ lib/Screens/Palm.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+
+
+class Palm extends StatefulWidget {
+  @override
+  _PalmState createState() => _PalmState();
+}
+
+class _PalmState extends State<Palm> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
