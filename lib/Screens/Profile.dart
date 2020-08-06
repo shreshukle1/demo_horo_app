864642f70febacc7f9f17ebbf81b73c245cf8fa5
Lincoln Kushwah <lincolnkushwah009@gmail.com>
2020-08-06 12:47:56 +0530

    code

--- lib/Screens/Profile.dart
+++ lib/Screens/Profile.dart
@@ -26,11 +26,11 @@ class _ProfileState extends State<Profile> {
     super.initState();
     FirebaseAuth auth = FirebaseAuth.instance;
     auth.currentUser().then((fu) => {
-          DatabaseServices.getUser(fu.email).then((_user) => {
-                setState(() => {user = _user})
-              })
-        });
-        print(user.dob);
+      DatabaseServices.getUser(fu.email).then((_user) => {
+        setState(() => {user = _user})
+      })
+    });
+    print(user.dob);
     return;
   }
 
@@ -41,143 +41,157 @@ class _ProfileState extends State<Profile> {
 
         body: new Stack(
 
-      children: <Widget>[
-        ClipPath(
-          child: Container(
-              color: Color.fromRGBO(0, 0, 51, 100)),
-          clipper: getClipper(),
-        ),
-        Positioned(
+          children: <Widget>[
+            ClipPath(
+              child: Container(
+                  color: Color.fromRGBO(0, 0, 51, 100)),
+              clipper: getClipper(),
+            ),
+            Positioned(
 
-          width: MediaQuery.of(context).size.width,
-            top: MediaQuery.of(context).size.height / 5,
-            child: Column(
-              children: <Widget>[
-                Container(
-                    alignment: Alignment.topCenter,
-                    width: 150.0,
-                    height: 150.0,
-                    decoration: BoxDecoration(
-                        color: Colors.transparent,
-                        image: DecorationImage(
-                            image: AssetImage("images/male.png"),
-                            fit: BoxFit.cover),
-                        borderRadius: BorderRadius.all(Radius.circular(75.0)),
-                        boxShadow: [
+                width: MediaQuery.of(context).size.width,
+                top: MediaQuery.of(context).size.height / 5,
+                child: Column(
+                  children: <Widget>[
+                    user.gender=='male'?
+                    Container(
+                        alignment: Alignment.topCenter,
+                        width: 150.0,
+                        height: 150.0,
+                        decoration: BoxDecoration(
+                            color: Colors.transparent,
+                            image: DecorationImage(
+                                image: AssetImage("images/male.png"),
+                                fit: BoxFit.cover),
+                            borderRadius: BorderRadius.all(Radius.circular(75.0)),
+                            boxShadow: [
 //                              BoxShadow(blurRadius: 7.0, color: Colors.black)
-                        ])),
+                            ])):
+                    Container(
+                        alignment: Alignment.topCenter,
+                        width: 150.0,
+                        height: 150.0,
+                        decoration: BoxDecoration(
+                            color: Colors.transparent,
+                            image: DecorationImage(
+                                image: AssetImage("images/female.png"),
+                                fit: BoxFit.cover),
+                            borderRadius: BorderRadius.all(Radius.circular(75.0)),
+                            boxShadow: [
+//                              BoxShadow(blurRadius: 7.0, color: Colors.black)
+                            ])),
 
-                Text(
-                  user.firstName,
-                  style: TextStyle(
-                    color: Colors.white,
-                      fontSize: 30.0,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Montserrat'),
-                ),
-                Container(
-                  padding: new EdgeInsets.all(8.0),
-                  child: new Center(
-                    child: new Column(
-                      children: <Widget>[
-                        new Card(
-                          child: new Container(
-                            child: new Column(
-                              children: <Widget>[
-                                new ListTile(
-                                  leading:
+                    Text(
+                      user.firstName,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30.0,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Montserrat'),
+                    ),
+                    Container(
+                      padding: new EdgeInsets.all(8.0),
+                      child: new Center(
+                        child: new Column(
+                          children: <Widget>[
+                            new Card(
+                              child: new Container(
+                                child: new Column(
+                                  children: <Widget>[
+                                    new ListTile(
+                                      leading:
                                       Icon(Icons.email, color: Color.fromRGBO(0, 0, 51, 100),),
-                                  title: Text("${user.email}",
-                                      style: TextStyle(
-                                        color: Colors.blueGrey,
-                                      )),
+                                      title: Text("${user.email}",
+                                          style: TextStyle(
+                                            color: Colors.blueGrey,
+                                          )),
 
 
-                                ),
-                                Padding(
-                                  padding:
+                                    ),
+                                    Padding(
+                                      padding:
                                       const EdgeInsets.fromLTRB(20, 0, 20, 0),
-                                  child: Divider(
-                                    height: 2,
-                                  ),
-                                ),
-                                ListTile(
-                                  leading: Icon(Icons.phone_android,
-                                      color: Color.fromRGBO(0, 0, 51, 100)),
-                                  title: Text("${user.phoneNumber}",
-                                      style: TextStyle(
-                                        color: Colors.blueGrey,
-                                      )),
-                                ),
-                                Padding(
-                                  padding:
+                                      child: Divider(
+                                        height: 2,
+                                      ),
+                                    ),
+                                    ListTile(
+                                      leading: Icon(Icons.phone_android,
+                                          color: Color.fromRGBO(0, 0, 51, 100)),
+                                      title: Text("${user.phoneNumber}",
+                                          style: TextStyle(
+                                            color: Colors.blueGrey,
+                                          )),
+                                    ),
+                                    Padding(
+                                      padding:
                                       const EdgeInsets.fromLTRB(20, 0, 20, 0),
-                                  child: Divider(
-                                    height: 2,
-                                  ),
-                                ),
-                                ListTile(
-                                  leading: Icon(Icons.cake, color: Color.fromRGBO(0, 0, 51, 100)),
-                                  title: Text(getFormattedDate(user.dob),
-                                      style: TextStyle(
-                                          color: Color.fromRGBO(0, 0, 51, 100)
-                                      )),
-                                ),
-                                Padding(
-                                  padding:
+                                      child: Divider(
+                                        height: 2,
+                                      ),
+                                    ),
+                                    ListTile(
+                                      leading: Icon(Icons.cake, color: Color.fromRGBO(0, 0, 51, 100)),
+                                      title: Text(getFormattedDate(user.dob),
+                                          style: TextStyle(
+                                              color: Color.fromRGBO(0, 0, 51, 100)
+                                          )),
+                                    ),
+                                    Padding(
+                                      padding:
                                       const EdgeInsets.fromLTRB(20, 0, 20, 0),
-                                  child: Divider(
-                                    height: 2,
-                                  ),
-                                ),
-                                ListTile(
-                                  onTap: () {
+                                      child: Divider(
+                                        height: 2,
+                                      ),
+                                    ),
+                                    ListTile(
+                                      onTap: () {
 //                                    Navigator.push(
 //                                        context,
 //                                        MaterialPageRoute(
 //                                            builder: (context) =>
 //                                                FormScreen()));
-                                  },
-                                  leading: Icon(
-                                    Icons.help,
-                                      color: Color.fromRGBO(0, 0, 51, 100)
-                                  ),
-                                  title: Text("Contact Us",
-                                      style: TextStyle(
-                                        color: Colors.blueGrey,
-                                      )),
-                                )
-                              ],
+                                      },
+                                      leading: Icon(
+                                          Icons.help,
+                                          color: Color.fromRGBO(0, 0, 51, 100)
+                                      ),
+                                      title: Text("Contact Us",
+                                          style: TextStyle(
+                                            color: Colors.blueGrey,
+                                          )),
+                                    )
+                                  ],
+                                ),
+                              ),
                             ),
-                          ),
+                          ],
                         ),
-                      ],
+                      ),
                     ),
-                  ),
-                ),
-                GestureDetector(
+                    GestureDetector(
 
-                  onTap: ()  async {
-        SharedPreferences prefs = await SharedPreferences.getInstance();
-        prefs.remove('_email');
-        Navigator.pushReplacement(context,
-        MaterialPageRoute(builder: (BuildContext ctx) => LoginPage ()));
-        },
+                      onTap: ()  async {
+                        SharedPreferences prefs = await SharedPreferences.getInstance();
+                        prefs.remove('_email');
+                        Navigator.pushReplacement(context,
+                            MaterialPageRoute(builder: (BuildContext ctx) => LoginPage ()));
+                      },
 
-                  child: Container(
-                    child: Text("LOGOUT",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            letterSpacing: 4,
-                            fontSize: 15,
-                          color: Colors.white
-                        )),
-                  ),
-                )
-              ],
-            ))
-      ],
-    ));
+                      child: Container(
+                        child: Text("LOGOUT",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                letterSpacing: 4,
+                                fontSize: 15,
+                                color: Colors.white
+                            )),
+                      ),
+                    )
+                  ],
+                ))
+          ],
+        ));
   }
 }
 
